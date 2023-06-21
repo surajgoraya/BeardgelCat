efee6f5560ef79c5a69e8db4d20d1ad3a1ca880d
surajgoraya <SurajGoraya@outlook.com>
2023-06-20 21:05:30 -0400

    feat: wake up message

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const { request } = require('undici');
 const cron = require('node-cron');
 
 const _TOKEN = process.env.DISCORD_TOKEN;
-const _SEND_CHANNEL = process.env.CHANNEL_ID;
 
-const { AttachmentBuilder, Client, Events, GatewayIntentBits, Activity, ActivityType } = require('discord.js');
+
+const { AttachmentBuilder, Client, Events, GatewayIntentBits, Activity, ActivityType, EmbedBuilder } = require('discord.js');
+const { MessageMainChannel, delay, MessageMainChannelWithEmbed } = require('./lib/common');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
 
@@ -21,14 +22,22 @@ client.once(Events.ClientReady, c => {
     client.user.setPresence({
         status: 'idle',
         afk: true,
-    })
+    });
+
+    delay(100);
+
+    const embedHelloGif = new EmbedBuilder()
+    .setColor('#FF8DC4')
+    .setTitle('That was a good nap :) - Back online, meow.')
+    .setImage('https://media1.giphy.com/media/9SVdZvlJYzTdS/giphy.gif');
+    
+    MessageMainChannelWithEmbed(c, embedHelloGif)
 
     /**
      * Automated job, runs at 8:30 daily - gets the dates for starfield.
      */
     cron.schedule("30 8 * * *", async () => {
-        const channel = c.channels.cache.find(channel => channel.id === _SEND_CHANNEL);
-        channel.send(`Morning @everyone, ${await get_starfield()}`);
+        MessageMainChannel(c, `Morning @everyone, ${await get_starfield()}`)
     })
 });
 
@@ -54,12 +63,10 @@ const get_starfield = async () => {
 
 process.on('SIGINT', async function () {
     console.log("Got SIGINT, shutting down.");
-    const channel = client.channels.cache.find(channel => channel.id === _SEND_CHANNEL);
-    channel.send(`Meow. The server is restarting, if I don't come back in a few minutes, please tell me I was a good kitty ❤️`);
+
+    MessageMainChannel(client, `Meow. The server is restarting, if I don't come back in a few minutes, please tell me I was a good kitty ❤️`);
+    
     await delay(2000);
     process.exit(0);
 });
 
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
