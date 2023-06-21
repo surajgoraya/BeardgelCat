b24a3dbaf8c4b71aef7fbfa305dbcd8989ab6189
surajgoraya <SurajGoraya@outlook.com>
2023-06-21 18:26:39 -0400

    feat: :sparkles: Added random replies, and sub dates for countdowns!

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const _SHOULD_BE_QUIET = process.env.QUIET_NOTIFY === 'yes';
 
 const { AttachmentBuilder, Client, Events, GatewayIntentBits, Activity, ActivityType, EmbedBuilder } = require('discord.js');
 const { MessageMainChannel, delay, MessageMainChannelWithEmbed } = require('./lib/common');
+const { UNKNOWN_COMMAND_REPLIES }  = require('./lib/replies');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
 
@@ -29,17 +31,25 @@ client.once(Events.ClientReady, c => {
         delay(100);
         const embedHelloGif = new EmbedBuilder()
         .setColor('#FF8DC4')
-        .setTitle('That was a good nap :) - Back online, meow.')
+        .setTitle('That was a good nap :) - Back online meow.')
         .setImage('https://media1.giphy.com/media/9SVdZvlJYzTdS/giphy.gif');
-        
-        MessageMainChannelWithEmbed(c, embedHelloGif)
+       
+        MessageMainChannelWithEmbed(c, embedHelloGif);
+
+        const revision = require('child_process')
+        .execSync(`git log -1 --pretty="format:%s"`)
+        .toString().trim()
+
+        MessageMainChannel(`I've learned new tricks!: ${revision} `)
     }
 
     /**
      * Automated job, runs at 8:30 daily - gets the dates for starfield.
      */
     cron.schedule("30 8 * * *", async () => {
-        MessageMainChannel(c, `Morning @everyone, ${await get_starfield()}`)
+        const starfield_message = await get_starfield();
+        MessageMainChannel(c, `Morning @everyone, ${starfield_message[0]}`)
+        MessageMainChannel(c, `${starfield_message[1]}`)
     })
 });
 
@@ -51,9 +61,10 @@ client.on(Events.MessageCreate, async (message) => {
 
     if (message.mentions.users.get(client.user.id) && message.author.id !== client.user.id) {
         if (message.content.toLowerCase().includes('starfield')) {
-            message.reply({ content: ` ${await get_starfield()} ${message.author}` });
+            message.reply({ content: ` ${await get_starfield()[0]} ${message.author}` });
         } else {
-            message.reply({ content: `Meow. ${message.author}` });
+            const RandomReply = UNKNOWN_COMMAND_REPLIES[Math.floor(Math.random() * array.length)];
+            message.reply({ content: `${RandomReply}` });
         }
     }
 });
@@ -62,7 +73,7 @@ const get_starfield = async () => {
     const req = await request('https://count.beardgel.ca/api/countdowns', { headers: { 'User-Agent': 'BeardgelCat/1.0.0' } })
     if (req.statusCode === 200) {
         const decoded = await req.body.json();
-        return `${decoded.Countdowns[0].text} ${decoded.Countdowns[0].friendly_date}, Meow!`
+        return [`${decoded.Countdowns[0].text} ${decoded.Countdowns[0].friendly_date}, Meow!`, `${decoded.Countdowns[0].sub_text} ${decoded.Countdowns[0].friendly_sub_date} `]
     } else {
         return 'Looks like my data source is down. meow. :('
     }
@@ -72,7 +83,7 @@ process.on('SIGINT', async function () {
     
     if(!_SHOULD_BE_QUIET){
         console.log("Got SIGINT, shutting down.");
-        MessageMainChannel(client, `Meow. The server is restarting, if I don't come back in a few minutes, please tell me I was a good kitty ❤️`);
+        MessageMainChannel(client, `Meow. The server is restarting, probably to get new updates, if I don't come back in a few minutes, please tell me I was a good kitty ❤️`);
         await delay(2000);
     }
 
